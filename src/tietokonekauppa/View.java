ef33b37b162f6e6e76204fb21cd9088f1f91d0d7
Rasmus Julin <rasmus.julin@metropolia.fi>
2019-01-25 10:59:58 +0200

    Lisää testejä...

--- src/tietokonekauppa/View.java
+++ src/tietokonekauppa/View.java
@@ -218,7 +218,7 @@ public class View extends Application {
 	}
                 
         private void pekka(){
-            
+            int pekka;
         }
 	public static void main(String[] args) {
 		launch(args);
